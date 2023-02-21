485276546fe0285cae7875ccaa995256c5fa123d
Vitaly1991 <convinceall@mail.ru>
2023-02-21 22:35:41 +0700

    tz9

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,39 +1,40 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import javax.validation.Valid;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.validator.UserValidator;
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-
+@RequestMapping("/users")
 @RestController
 @Slf4j
 @Getter
 public class UserController {
-    private final Map<Integer, User> users = new HashMap<Integer, User>();
+    private final Map<Integer, User> users = new HashMap<>();
      UserValidator userValidator;
-    @GetMapping("/users")
+     private int IdCount;
+    @GetMapping
     public Collection<User> findAll() {
         log.info("Получен запрос на получение списка пользователей");
         return users.values();
     }
 
 
-    @PostMapping("/users")
-    public User create(@Valid @RequestBody User user) {
+    @PostMapping
+    public User create(@RequestBody User user) {
         userValidator.validater(user);
+        user.setId(IdCount);
+        IdCount++;
         log.info("Вы - {}!", "добавили нового пользователя");
         users.put(user.getId(), user);
         return user;
     }
 
-    @PutMapping("/users")
-    public User putUser(@Valid @RequestBody User user) {
+    @PutMapping
+    public User putUser(@RequestBody User user) {
         userValidator.validater(user);
         log.info("Вы - {}!", " обновили данные для текущего фильма");
         users.put(user.getId(), user);
